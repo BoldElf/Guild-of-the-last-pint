7168e1fce84b0a61bb7e9078c25cf166b281fa8a
BoldElf <denis.den.kovalenko11@mail.ru>
2025-02-16 22:02:07 +0300

    settings

--- Assets/Scripts/Enemy/EnemyDetect.cs
+++ Assets/Scripts/Enemy/EnemyDetect.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using Zenject.SpaceFighter;
 using Zenject;
 
