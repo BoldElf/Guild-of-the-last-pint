7a1efb150f71d5850de92916eaf1da159ac82033
BoldElf <denis.den.kovalenko11@mail.ru>
2025-02-16 11:59:18 +0300

    Add restar

--- Assets/Scripts/Enemy/EnemyDetectController.cs
+++ Assets/Scripts/Enemy/EnemyDetectController.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
 
 public class EnemyDetectController : MonoBehaviour
 {
+    [Inject] private DiContainer _container;
     private bool detect = false;
 
-   public void playerDetect()
-   {
-        /*
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private Canvas canvas;
+
+    private GameObject buttonRestart;
+
+    public void playerDetect()
+    {
         if(detect == false)
         {
-            Debug.Log("Player detect");
-            //detect = true;
-        }
-        */
+            Time.timeScale = 0;
 
-        Debug.Log("Player detect");
+            buttonRestart = _container.InstantiatePrefab(buttonPrefab, canvas.transform);
+            buttonRestart.GetComponent<Button>().onClick.AddListener(Restart);
+            Debug.Log("Player detect");
+            detect = true;
+        }
+        
+    }
+
+    private void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
