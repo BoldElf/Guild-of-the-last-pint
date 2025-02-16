2289d549ec3bb59f4d79eccae67ac65b89e376b3
DanilaMax <danilakazakovmax@gmail.com>
2025-02-16 15:21:17 +0300

    Commit Next
    Добавлены звуки ходьбы для ученых.

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,39 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using Unity.AI;
 using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] private Transform patroleRote;
-
+    [SerializeField] private Transform patrolRoute;
+    [SerializeField] private AudioClip movingSound; 
     private List<Transform> targets = new List<Transform>();
-
     private int localIndex = 0;
     private NavMeshAgent agent;
+    private AudioSource audioSource;
+    private bool isPlayingMovingSound = false;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
 
-        InitializePatroleRoute();
+        audioSource.clip = movingSound;
+        audioSource.loop = true; 
 
+        InitializePatrolRoute();
         MoveToNextLocation();
     }
 
     private void Update()
     {
-        if(agent.remainingDistance < 0.2f && !agent.pathPending)
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
         {
             MoveToNextLocation();
         }
+
+        if (agent.velocity.magnitude > 0.1f)
+        {
+            if (!isPlayingMovingSound)
+            {
+                audioSource.Play();
+                isPlayingMovingSound = true;
+            }
+        }
+        else
+        {
+            if (isPlayingMovingSound)
+            {
+                audioSource.Stop();
+                isPlayingMovingSound = false;
+            }
+        }
     }
 
-    private void InitializePatroleRoute()
+    private void InitializePatrolRoute()
     {
-        foreach(Transform route in patroleRote)
+        foreach (Transform route in patrolRoute)
         {
             targets.Add(route);
         }
@@ -41,7 +64,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     private void MoveToNextLocation()
     {
-        if(targets.Count > 0)
+        if (targets.Count > 0)
         {
             agent.destination = targets[localIndex].position;
             localIndex = (localIndex + 1) % targets.Count;
