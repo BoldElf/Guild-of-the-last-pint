36d875e7d8e439f3c0568b306c38039917299b17
DanilaMax <danilakazakovmax@gmail.com>
2025-02-16 19:45:49 +0300

    Commit Next
    окончательное меню.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,11 +6,7 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject pausePanel; 
     [SerializeField] private GameObject resumeButton; 
     [SerializeField] private GameObject exitButton; 
-    [SerializeField] private GameObject proofsButton; 
-    [SerializeField] private GameObject proofsPanelPrefab; 
-    private GameObject proofsPanelInstance;
     private bool isPaused = false;
-    private bool isProofsPanelActive = false;
 
     private void Start()
     {
@@ -31,7 +27,6 @@ public class PauseMenu : MonoBehaviour
 
         if (isPaused)
         {
-
             pausePanel.SetActive(true);
             Time.timeScale = 0; 
             AudioListener.pause = true; 
@@ -39,10 +34,6 @@ public class PauseMenu : MonoBehaviour
         else
         {
             pausePanel.SetActive(false);
-            if (proofsPanelInstance != null)
-            {
-                proofsPanelInstance.SetActive(false);
-            }
             Time.timeScale = 1; 
             AudioListener.pause = false; 
         }
@@ -59,27 +50,5 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1; 
         AudioListener.pause = false; 
         SceneManager.LoadScene(0);
-    }
-
-    public void ToggleProofsPanel()
-    {
-        isProofsPanelActive = !isProofsPanelActive;
-
-        if (isProofsPanelActive)
-        {
-            if (proofsPanelInstance == null)
-            {
-                proofsPanelInstance = Instantiate(proofsPanelPrefab, pausePanel.transform);
-            }
-            proofsPanelInstance.SetActive(true);
-        }
-        else
-        {
-            if (proofsPanelInstance != null)
-            {
-                Destroy(proofsPanelInstance);
-                proofsPanelInstance = null;
-            }
-        }
     }
 }
